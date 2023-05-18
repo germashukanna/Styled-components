79860d04d8e579885272b569413bf2f41dacc3d8
germashukanna <50459965+germashukanna@users.noreply.github.com>
2023-05-18 15:39:23 +0300

    initial commit

---
+++ src/components/Title.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-family: 'Inter', sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  //margin: 0 20px 20px 20px;
+`
